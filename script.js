ef9a7cd622014d8042922bcf56fd1ff15b9e03b9
Vlad <vlad96homenko908@gmail.com>
2020-02-27 11:30:17 +0200

    add coment a comment

--- script.js
+++ script.js
@@ -10,24 +10,26 @@ let appData = {
   timeData: time,
   savings: false
 };
-// for (let i = 0; i < 2; i++) {
-//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//     b = prompt("Во сколько обойдется?", "");
-//   if (
-//     typeof a === "string" &&
-//     typeof a != null &&
-//     typeof b != null &&
-//     a != "" &&
-//     b != "" &&
-//     a.length < 50
-//   ) {
-//     console.log("done");
-//     appData.expenses[a] = b;
-//   } else {
-//     console.log ("bad result");
-//         i--;
-//   }
-// }
+//for
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt("Во сколько обойдется?", "");
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    console.log("done");
+    appData.expenses[a] = b;
+  } else {
+    console.log("bad result");
+    i--;
+  }
+}
+
 //while
 // let i = 0;
 // while (i < 2) {
@@ -49,27 +51,28 @@ let appData = {
 //   }
 //   i++;
 // }
+
 //do while
-let i = 0;
-do {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    b = prompt("Во сколько обойдется?", "");
-  if (
-    typeof a === "string" &&
-    typeof a != null &&
-    typeof b != null &&
-    a != "" &&
-    b != "" &&
-    a.length < 50
-  ) {
-    console.log("done");
-    appData.expenses[a] = b;
-  } else {
-    console.log("bad result");
-    i--;
-  }
-  i++;
-} while (i < 2);
+// let i = 0;
+// do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     b = prompt("Во сколько обойдется?", "");
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 50
+//   ) {
+//     console.log("done");
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log("bad result");
+//     i--;
+//   }
+//   i++;
+// } while (i < 2);
 
 appData.moneyPerDay = appData.budget / 30;
 alert("Eжедневный бюджет:" + appData.moneyPerDay);
